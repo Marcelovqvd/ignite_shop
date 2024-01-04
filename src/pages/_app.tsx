7217fe19713a9c3c7652454897d5fdc599522718
Marcelo <marcelovqvd@gmail.com>
2024-01-04 12:13:36 -0300

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import { globalStyles } from '../styles/global';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+
+globalStyles();
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
+
+export default App;
