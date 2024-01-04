c7f383d5354ed81c6a1c2cc8259754d04d5b78fc
Marcelo <marcelovqvd@gmail.com>
2024-01-03 21:26:05 -0300

    Estrutura visual - Configurando Stitches

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import { styled } from '../styles';
 
-export default function index() {
-  return <div>index</div>;
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+});
+
+export default function Home() {
+  return (
+    <Button>
+      <h1>teste</h1>
+    </Button>
+  );
 }
